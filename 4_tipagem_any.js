2251d1b04041cdb55a5667bc1a3587a61c37fe53
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-05 15:29:36 -0300

    Tipando com any, ele aceita qualquer valor para a variável

---
+++ 4_tipagem_any.js
@@ -0,0 +1,7 @@
+var info;
+info = [1, 2, 3];
+info = "Eduardo";
+info = true;
+info = 10.2;
+
+console.log(info);
---
+++ 4_tipagem_any.ts
@@ -0,0 +1,5 @@
+let info: any;
+info = [1,2,3]
+info = "Eduardo"
+info = true
+info = 10.2
