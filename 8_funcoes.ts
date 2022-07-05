112ad707824551bb160d1818d0729b790fc71ba1
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-05 15:48:25 -0300

    Tipos de return em funções

---
+++ 8_funcoes.ts
@@ -0,0 +1,4 @@
+function showMessages(message: string): void { // o tipo de retorno pode ser explicito ou implicito não é necessario passar um tipo
+  console.log(message);
+  // return message; // Irá retornar erro pois é do tipo void
+}
